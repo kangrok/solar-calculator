8bd67f271461f227856617b2ec247e76bb858112
Külli Kangro <kangro.kylli@gmai..com>
2020-04-22 17:46:44 +0300

    add prod files

---
+++ build/precache-manifest.89e82f7ee8d642d1454b96291d14d9a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d11296109232dd39d61fc773fe5acf03",
+    "url": "/index.html"
+  },
+  {
+    "revision": "983676f0ccc16ee64a60",
+    "url": "/static/css/main.81975396.chunk.css"
+  },
+  {
+    "revision": "e39930a521b53277978d",
+    "url": "/static/js/2.00669185.chunk.js"
+  },
+  {
+    "revision": "2cb8af4c82e5e0c1d5ca61b87389414c",
+    "url": "/static/js/2.00669185.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "983676f0ccc16ee64a60",
+    "url": "/static/js/main.378b6ada.chunk.js"
+  },
+  {
+    "revision": "bd94aa4863b422f0bf28",
+    "url": "/static/js/runtime-main.d5ed6300.js"
+  }
+]);
