ad90a0a684ca38912f62f15f3e9a92750ab9c0c9
Külli Kangro <kangro.kylli@gmai..com>
2020-04-22 17:58:34 +0300

    Updates

---
+++ precache-manifest.1d3662c3ca7afe061bfaeeda2a2a3f91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "315bd2a67336deabc33d4d985d588804",
+    "url": "/solar-calculator/index.html"
+  },
+  {
+    "revision": "2e6107f4b41bc6feb0d9",
+    "url": "/solar-calculator/static/css/main.81975396.chunk.css"
+  },
+  {
+    "revision": "17f5fcbec78ee5e6d533",
+    "url": "/solar-calculator/static/js/2.1b59b977.chunk.js"
+  },
+  {
+    "revision": "2cb8af4c82e5e0c1d5ca61b87389414c",
+    "url": "/solar-calculator/static/js/2.1b59b977.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e6107f4b41bc6feb0d9",
+    "url": "/solar-calculator/static/js/main.10bea9e3.chunk.js"
+  },
+  {
+    "revision": "d711af06ed6c02433549",
+    "url": "/solar-calculator/static/js/runtime-main.a36794cb.js"
+  }
+]);
